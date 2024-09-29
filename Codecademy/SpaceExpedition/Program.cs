3b9b91f353d11856767dff8427b96643e08d4771
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-09-30 00:06:18 +0700

    add: Space Expedition

--- Codecademy/SpaceExpedition/Program.cs
+++ Codecademy/SpaceExpedition/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.ComponentModel;
 
 public class SpaceExpedition
 {
   public static void Main(string[] args)
   {
-    // Your code goes here
+    bool isAtmosphereBreathable = true;
+    bool isGravityStable = false;
+    bool resourcesSufficient = true;
 
+    bool isHabitable = isAtmosphereBreathable && isGravityStable;
+    Console.WriteLine($"Habitable? : {isHabitable}");
+
+    bool suitableForExpansion = isGravityStable && resourcesSufficient;
+    Console.WriteLine($"Suitable for Expansion? : {suitableForExpansion}");
+
+    int currentCrew = 5;
+    int maxCrewCapacity = 10;
+
+    bool canAcceptMoreCrew = currentCrew < maxCrewCapacity;
+    Console.WriteLine($"Can Accept More Crew? : {canAcceptMoreCrew}");
   }
 }
