2eb7e4eb5bc87256ba5ed0dfd33262d2ab77807d
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-10-09 13:40:42 +0700

    add: Corporate Task 1-3

--- Codecademy/Corporate/Employee.cs
+++ Codecademy/Corporate/Employee.cs
@@ -2,5 +2,20 @@ using System;
 
 namespace CorporatePolymorphism
 {
-  // Placeholder for Employee class implementation
+  public abstract class Employee
+  // abstract class = whatever inherit from this has to implement whatever flagged abstract
+  {
+    public abstract void SubmitDailyReport();
+    // inherited class has to define this method to its own
+    public virtual void Work()
+    // inherited class can override this method
+    {
+      Console.WriteLine("Employee is working.");
+    }
+    public void ClockIn()
+    // this only inherit and cannot be override
+    {
+      Console.WriteLine("Employee clocks in.");
+    }
+  }
 }
